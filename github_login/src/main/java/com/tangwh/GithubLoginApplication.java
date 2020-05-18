35d3d8076f0f0cc80fe223287b6cc7f45b924b10
samll-howe <2678691035@qq.com>
2020-05-18 21:44:25 +0800

    对接GitHub第三方登录

---
+++ github_login/src/main/java/com/tangwh/GithubLoginApplication.java
@@ -0,0 +1,24 @@
+package com.tangwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class GithubLoginApplication {
+
+    public static void main(String[] args) {
+
+
+
+        SpringApplication.run(GithubLoginApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate(){
+
+        return new RestTemplate();
+    }
+
+}
